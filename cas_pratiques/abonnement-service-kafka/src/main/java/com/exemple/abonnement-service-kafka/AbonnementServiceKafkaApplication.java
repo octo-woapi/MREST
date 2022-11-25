c1b300989390c30644eb180410a708e1a6c8056f
Christophe Charles <christophe.charles@octo.com>
2022-11-25 20:11:21 +0100

    feat: création du service Abonnement utilisant kafka

---
+++ cas_pratiques/abonnement-service-kafka/src/main/java/com/exemple/abonnement-service-kafka/AbonnementServiceKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.exemple.abonnementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbonnementServiceKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbonnementServiceKafkaApplication.class, args);
+	}
+
+}
---
+++ cas_pratiques/abonnement-service-kafka/src/test/java/com/exemple/abonnement-service-kafka/AbonnementServiceKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemple.abonnementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbonnementServiceKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
